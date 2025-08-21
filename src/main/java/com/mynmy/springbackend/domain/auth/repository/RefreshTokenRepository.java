83c42f7684308aca8156eaeb44c681a1319927d8
Soyeon <isylsy166@gmail.com>
2025-08-21 14:16:18 +0900

    fix: Auth 토큰 삭제에 @Transactional @Modifying 추가

--- src/main/java/com/mynmy/springbackend/domain/auth/repository/RefreshTokenRepository.java
+++ src/main/java/com/mynmy/springbackend/domain/auth/repository/RefreshTokenRepository.java
@@ -1,12 +1,17 @@
 package com.mynmy.springbackend.domain.auth.repository;
 
 import com.mynmy.springbackend.domain.auth.entity.RefreshToken;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 
 import java.util.Optional;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByToken(String token);
     void deleteByToken(String token);
+
+    @Transactional
+    @Modifying
     void deleteByUserId(Long userId);
 }
